cef1adfb97b10293cd7750f996397250c677ffa4
eclonghurst <emily.longhurst3@sky.uk>
2023-09-08 12:29:13 +0100

    added Variables package

---
+++ firstProject/src/VariablesExercise/Variables.java
@@ -0,0 +1,24 @@
+package VariablesExercise;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        /*String firstName = "Em";
+        String lastName = "Long";
+        String name = firstName + lastName;
+        System.out.println(name);*/
+
+        float num1 = 4F;
+        float num2 = 16F;
+        System.out.println(num1 + num2);
+        float result = num1 / num2;
+        System.out.println(result);
+
+        int num3 = 8;
+        System.out.println(num3);
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+        // difference is that num3++ prints then adds, ++num3 adds then prints
+    }
+}
--- firstProject/src/intro/Runner.java
+++ firstProject/src/intro/Runner.java
@@ -3,5 +3,14 @@ package intro;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int sizeOf = 5;
+        int big = sizeOf * sizeOf * sizeOf;
+        System.out.println(big);
+        long bigger = big + big + big;
+        System.out.println(bigger);
+        double old = 78.0;
+        System.out.println(old);
+        boolean max = big > bigger;
+        System.out.println(max);
         }
     }
